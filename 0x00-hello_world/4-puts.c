9ee5a824ccab51dd0daf88e01e4f6c83a3d21204
Joan <joannawaweru363@gmail.com>
2022-03-12 16:18:46 +0300

    Puts phrase

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
- /**
- * main - prints the phrase "Programming is like building a multilingual puzzle 
- * Return - should be 0 when exited
+ /** 
+ * Return - should be 0 when executed
  */
 int main(void)
+ /**
+  * main - prints the phrase "Programming is like building a multilingual puzzle
+  */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
